6affcf26d9032d414d4854f2c3c0132c874e6bc8
DZ in The House <icelffs@gmail.com>
2018-08-04 21:13:19 +0800

    remove empty line

--- fs.js
+++ fs.js
@@ -1,7 +1,6 @@
 fs = require('fs');
 
 module.exports.writeFile = function (fileName, fileContent) {
-
     fs.writeFile(fileName, fileContent, (err) => {
         if (err)
             console.log(err);
